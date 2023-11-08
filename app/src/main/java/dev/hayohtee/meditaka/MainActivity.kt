23be02ce12d0bb9b2d1387ac8d9a6010b408535a
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2023-11-08 17:39:58 +0100

    Call installSplashScreen() in MainActivity.kt

--- app/src/main/java/dev/hayohtee/meditaka/MainActivity.kt
+++ app/src/main/java/dev/hayohtee/meditaka/MainActivity.kt
@@ -7,11 +7,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import dev.hayohtee.meditaka.ui.theme.MeditakaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             MeditakaTheme {
                 // A surface container using the 'background' color from the theme
